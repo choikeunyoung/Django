828875db12db9a03b87aedf25a5c3447d9e4978c
choikeunyoung <ought119@gmail.com>
2022-10-11 17:09:41 +0900

    회원가입 만들기

--- 1011_실습/information/forms.py
+++ 1011_실습/information/forms.py
@@ -1,2 +1,8 @@
-from django.forms import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username',)
--- 1011_실습/information/views.py
+++ 1011_실습/information/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 def log_in(request):
-    form = UserCreationForm()
+    # POST 요청 처리
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
     context = {
         'form' : form
     }
